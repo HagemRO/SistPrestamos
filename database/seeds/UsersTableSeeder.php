575aa4aa4da9baaba995be8b137885df1722cd4e
HagemRO <41352730+HagemRO@users.noreply.github.com>
2022-02-19 19:43:39 -0800

    all

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,50 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    public function run()
+    {
+        $users = [
+            [
+                'id'             => 1,
+                'name'           => 'Trevor Admin Stanbridge',
+                'email'          => 'admin@admin.com',
+                'password'       => '$2y$10$PadOOF6GiHJqI1IQhPZNjeXkKGPip9vJXdhB5ra6lrvZdcZFZDCjy',
+                'remember_token' => null,
+            ],
+            [
+                'id'             => 2,
+                'name'           => 'John Analyst Stone',
+                'email'          => 'analyst@analyst.com',
+                'password'       => '$2y$10$PadOOF6GiHJqI1IQhPZNjeXkKGPip9vJXdhB5ra6lrvZdcZFZDCjy',
+                'remember_token' => null,
+            ],
+            [
+                'id'             => 3,
+                'name'           => 'Mia CFO Wong',
+                'email'          => 'cfo@cfo.com',
+                'password'       => '$2y$10$PadOOF6GiHJqI1IQhPZNjeXkKGPip9vJXdhB5ra6lrvZdcZFZDCjy',
+                'remember_token' => null,
+            ],
+            [
+                'id'             => 4,
+                'name'           => 'Adrian',
+                'email'          => 'cliente@cliente.co',
+                'password'       => '$2y$10$PadOOF6GiHJqI1IQhPZNjeXkKGPip9vJXdhB5ra6lrvZdcZFZDCjy',
+                'remember_token' => null,
+            ],
+            [
+                'id'             => 5,
+                'name'           => 'Juan',
+                'email'          => 'cobrador@cobrador.co',
+                'password'       => '$2y$10$PadOOF6GiHJqI1IQhPZNjeXkKGPip9vJXdhB5ra6lrvZdcZFZDCjy',
+                'remember_token' => null,
+            ],
+        ];
+
+        User::insert($users);
+    }
+}
